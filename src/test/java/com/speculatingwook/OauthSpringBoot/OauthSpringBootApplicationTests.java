ee42fabd8f0a7de2d6d946b9a3aa51b31a80e154
Brian <bwook9908@gmail.com>
2023-05-30 14:45:37 +0900

    feat: project start

---
+++ src/test/java/com/speculatingwook/OauthSpringBoot/OauthSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speculatingwook.OauthSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
